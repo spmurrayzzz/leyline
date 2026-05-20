6eecfdc7eaf4767f2cf32e1d60e5090098470e37
Stephen Murray <stephen.patrick.murray@gmail.com>
2026-05-19 21:59:02 -0400

    fix: make screenshot script work with SSE

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -5,7 +5,8 @@ const path = process.env.SCREENSHOT_PATH || 'screenshots/current.png'
 
 const browser = await chromium.launch()
 const page = await browser.newPage({ viewport: { width: 1503, height: 818 } })
-await page.goto(url, { waitUntil: 'networkidle' })
+await page.goto(url, { waitUntil: 'domcontentloaded' })
+await page.waitForTimeout(2500)
 await page.screenshot({ path, fullPage: true })
 await browser.close()
 
