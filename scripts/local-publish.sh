fc65600c90db83e777a8533b7a255ed29f870080
Stephen Murray <stephen.patrick.murray@gmail.com>
2026-07-11 12:28:06 -0400

    chore(scripts): cleanup initial release binary

--- scripts/local-publish.sh
+++ scripts/local-publish.sh
@@ -3,3 +3,4 @@ npm run electron:build
 rsync -a ./release/Leyline-darwin-arm64/Leyline.app /Applications/
 mkdir -p "$HOME/.local/bin"
 ln -sf "$(pwd)/bin/leyline" "$HOME/.local/bin/leyline"
+rm -rf release/Leyline-darwin-arm64
